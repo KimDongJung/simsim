0af76b39466697937454b1bcf4da99cb9fa47986
KimDongJung <gd30.c.kimu@gmail.com>
2023-02-21 18:52:33 +0900

    UPDATE -- 機能のクラス化

--- js-form-auto-complete-for-dev/form-auto-complete-for-dev.js
+++ js-form-auto-complete-for-dev/form-auto-complete-for-dev.js
@@ -1,74 +1,118 @@
 'use strict';
 
-// window.addEventListener('load', function () {
-//   const new_test_text_input_element = document.createElement('button');
-//   const new_test_text_input_element_event_handler = function (event) {
-//     const inputs = {
-//       text: {
-//         uname: "金",
-//         uage: "31",
-//       },
-//       checked: {
-//         ugender: '女性',
-//         uhabit: 'ゲーム',
-//         uhabit: '運動',
-//       },
-//       selected: {
-//         uplace: '沖縄',
-//       },
-//     };
+window.addEventListener('load', function () {
+  const new_test_text_input_element = document.createElement('button');
+  const new_test_text_input_element_event_handler = function (event) {
+    const inputs = {
+      text: {
+        uname: "金",
+        uage: "31",
+      },
+      checked: {
+        ugender: '女性',
+        uhabit: 'ゲーム',
+        uhabit: '運動',
+      },
+      selected: {
+        uplace: '沖縄',
+      },
+    };
     
-//     for (const type in inputs) {
-//       for (const input_name in inputs[type]) {
-//         let target_element; 
+    for (const type in inputs) {
+      for (const input_name in inputs[type]) {
+        let target_element; 
 
-//         switch (type) {
-//           case 'text':
-//             target_element = document.querySelector(`input[name=${input_name}]`);
-//             target_element.value = inputs[type][input_name];
-//             break;
-//           case 'checked':
-//             target_element = document.querySelector(`input[name="${input_name}"][value="${inputs[type][input_name]}"]`);
-//             target_element.checked = true;
-//             break;
-//           case 'selected':
-//             target_element = document.querySelector(`select[name=${input_name}]`);
-//             // const target_element_children_elements = target_element.children; // ① 方法
-//             const target_element_children_elements = target_element.options; // ② 方法
-//             /**
-//              * HTMLCollectionであるtarget_element_children_elementsはforEachメソッドは使えない
-//              * だが、ArrayライクObjectなので
-//              *  Array.prototype.forEach.call(target_element_children_elements, function ..) か
-//              *  for (const value of target_element_children_elements) ...
-//              * という形で使用できる
-//              */
-//             for (const target_element_children_element of target_element_children_elements) {
-//               const is_equal_request_data = target_element_children_element.value === inputs[type][input_name] ? true : false;
+        switch (type) {
+          case 'text':
+            target_element = document.querySelector(`input[name=${input_name}]`);
+            target_element.value = inputs[type][input_name];
+            break;
+          case 'checked':
+            target_element = document.querySelector(`input[name="${input_name}"][value="${inputs[type][input_name]}"]`);
+            target_element.checked = true;
+            break;
+          case 'selected':
+            target_element = document.querySelector(`select[name=${input_name}]`);
+            // const target_element_children_elements = target_element.children; // ① 方法
+            const target_element_children_elements = target_element.options; // ② 方法
+            /**
+             * HTMLCollectionであるtarget_element_children_elementsはforEachメソッドは使えない
+             * だが、ArrayライクObjectなので
+             *  Array.prototype.forEach.call(target_element_children_elements, function ..) か
+             *  for (const value of target_element_children_elements) ...
+             * という形で使用できる
+             */
+            for (const target_element_children_element of target_element_children_elements) {
+              const is_equal_request_data = target_element_children_element.value === inputs[type][input_name] ? true : false;
               
-//               if (is_equal_request_data) target_element_children_element.selected = true;
-//             }
-//             break;
-//         }
-//       }
-//     }
-//   };
+              if (is_equal_request_data) target_element_children_element.selected = true;
+            }
+            break;
+        }
+      }
+    }
+  };
 
-//   new_test_text_input_element.id = 'form-auto-complete-for-dev-btn';
-//   new_test_text_input_element.type = 'button';
-//   new_test_text_input_element.textContent = 'フォーム入力';
-//   new_test_text_input_element.style = 'position: fixed; z-index: 999; left: 0; bottom: 0; background-color: red; color: white;';
-//   document.body.appendChild(new_test_text_input_element);
-//   new_test_text_input_element.addEventListener('click', new_test_text_input_element_event_handler);
-// });
+  new_test_text_input_element.id = 'form-auto-complete-for-dev-btn';
+  new_test_text_input_element.type = 'button';
+  new_test_text_input_element.textContent = 'フォーム入力';
+  new_test_text_input_element.style = 'position: fixed; z-index: 999; left: 0; bottom: 0; background-color: red; color: white;';
+  document.body.appendChild(new_test_text_input_element);
+  new_test_text_input_element.addEventListener('click', new_test_text_input_element_event_handler);
+});
 
 class FormAutoCompleteForDev {
-  constructor () {
+  constructor (input_obj) {
     const new_test_text_input_element = document.createElement('button');
     new_test_text_input_element.id = 'form-auto-complete-for-dev-btn';
     new_test_text_input_element.type = 'button';
     new_test_text_input_element.textContent = 'フォーム入力';
     new_test_text_input_element.style = 'position: fixed; z-index: 999; left: 0; bottom: 0; background-color: red; color: white;';
     document.body.appendChild(new_test_text_input_element);
-    // new_test_text_input_element.addEventListener('click', new_test_text_input_element_event_handler);
+    this.autoCompleteButton = new_test_text_input_element
+  }
+
+  setInputObj (input_obj) {
+    this.inputObj = input_obj;
+  }
+
+  excute () {
+    const inputs = this.inputObj;
+    this.autoCompleteButton.addEventListener('click', function () {
+      for (const type in inputs) {
+        for (const input_name in inputs[type]) {
+          let target_element; 
+  
+          switch (type) {
+            case 'text':
+              target_element = document.querySelector(`input[name=${input_name}]`);
+              target_element.value = inputs[type][input_name];
+              break;
+            case 'checked':
+              target_element = document.querySelector(`input[name="${input_name}"][value="${inputs[type][input_name]}"]`);
+              target_element.checked = true;
+              break;
+            case 'selected':
+              target_element = document.querySelector(`select[name=${input_name}]`);
+              // const target_element_children_elements = target_element.children; // ① 方法
+              const target_element_children_elements = target_element.options; // ② 方法
+              /**
+               * HTMLCollectionであるtarget_element_children_elementsはforEachメソッドは使えない
+               * だが、ArrayライクObjectなので
+               *  Array.prototype.forEach.call(target_element_children_elements, function ..) か
+               *  for (const value of target_element_children_elements) ...
+               * という形で使用できる
+               */
+              for (const target_element_children_element of target_element_children_elements) {
+                const is_equal_request_data = target_element_children_element.value === inputs[type][input_name] ? true : false;
+                
+                if (is_equal_request_data) target_element_children_element.selected = true;
+              }
+              break;
+          }
+        }
+      }
+    });
+    
   }
 }
--- js-form-auto-complete-for-dev/script.js
+++ js-form-auto-complete-for-dev/script.js
@@ -1,4 +1,19 @@
 'use strict';
 window.addEventListener('load', function () {
-  new FormAutoCompleteForDev();
+  const auto_complete = new FormAutoCompleteForDev();
+  auto_complete.setInputObj({
+    text: {
+      uname: "金",
+      uage: "31",
+    },
+    checked: {
+      ugender: '女性',
+      uhabit: 'ゲーム',
+      uhabit: '運動',
+    },
+    selected: {
+      uplace: '沖縄',
+    },
+  });
+  auto_complete.excute();
 });
